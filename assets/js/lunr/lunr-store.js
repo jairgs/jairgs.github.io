2585b5f4955eaba516f30af77fcafae7dae8c1a8
jairgs <jair_fulgore@hotmail.com>
2018-10-25 00:17:48 -0500

    hide quickstart menu

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,21 @@
+var store = [{
+        "title": "Statistical Inference And Simulated Properties Of Some Statistics",
+        "excerpt":"Abstract In this document we present some aplications of statistical inference methods on real data. First we explore the most impotant theory on statistical inference, the Central Limit Theorem and show some difference between theoretical and sample statistics using an exponential distribution. Second, we explore the ToothGrowth database which features...","categories": [],
+        "tags": ["data-science","statistics","inference","plots"],
+        "url": "https://jairgs.github.io/Statistical-Inference-and-Simulated-Properties-of-some-statistics/",
+        "teaser":null},{
+        "title": "Principal Meteorological Events Responsible Of Fatalities, Injuries And Property Damage",
+        "excerpt":"Abstract Some recent interest in meteorological events and their economic and health consequences motivate this quantitative analysis to identify such events in order of importance and quantify its economic and health effects on the general population. We find that Hurricanes are the most expensive events with respect to property damage...","categories": [],
+        "tags": ["meteorology","regression","data-science","inference","plots"],
+        "url": "https://jairgs.github.io/Principal-meteorological-events-responsible-of-fatalities,-injuries-and-property-damage/",
+        "teaser":null},{
+        "title": "Regression Analysis: a study of the effect of transmission type to gasoline consumption in cars.",
+        "excerpt":"Executive Summary Regression analysis is one of the most important tools for a data scientist and statisticians in general since it provides a simple yet insightful way to model the underlying dynamics of the data. In this analysis, we show some strategies to model the Miles/gallon as a function of...","categories": [],
+        "tags": [],
+        "url": "https://jairgs.github.io/Regression-Analysis/",
+        "teaser":null},{
+        "title": "Machine Lerning Methods: an application building a correct dumbell curls classifier",
+        "excerpt":"Introduction With the advent of devices such as Jawbone Up, Nike FuelBand and Fitbit, it is increasibly easy and cheap for people to monitor and report body movements in order to improve their health, find patterns in their habits and just because is fun. With this devices comes an increasingly...","categories": [],
+        "tags": [],
+        "url": "https://jairgs.github.io/machine-learning/",
+        "teaser":null}]
